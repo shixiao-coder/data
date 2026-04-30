e2450573c98f2b7ef783c629f252ee67d94fca81
shixiao-coder <shixiao@google.com>
2026-04-30 11:50:57 -0400

    Bugfix for test

--- import-automation/workflow/embedding-helper/test/main_test.py
+++ import-automation/workflow/embedding-helper/test/main_test.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 import unittest
 from unittest.mock import MagicMock, patch
 from datetime import datetime
